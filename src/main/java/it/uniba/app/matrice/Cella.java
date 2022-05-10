5278ff29930c8ba169b4b686ff5d5d764aa87505
Verna-Vito <v.verna5@studenti.uniba.it>
2022-05-10 20:53:06 +0200

    Fix del package per la classe Cella

--- src/main/java/it/uniba/app/matrice/Cella.java
+++ src/main/java/it/uniba/app/matrice/Cella.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package it.uniba.app;
+package it.uniba.app.matrice;
 
 /**
  *
