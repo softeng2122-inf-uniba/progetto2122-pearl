2dc548739e8a9f72c72d222b4861ff9d9be1bf63
Verna-Vito <v.verna5@studenti.uniba.it>
2022-05-11 15:42:18 +0200

    Apportate modifiche richieste

--- src/main/java/it/uniba/app/parser/Comando.java
+++ src/main/java/it/uniba/app/parser/Comando.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package it.uniba.app.parser;
 
 /**
  *
- * @author verna
+ * @author Vito Verna - 746463
  */
 public class Comando {
     
