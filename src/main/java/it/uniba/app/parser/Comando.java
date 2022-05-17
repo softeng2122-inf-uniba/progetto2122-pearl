669fdb7ee7770c19490fb22fce51ec41a4a47fc1
SSUPII <42090377+SSUPII@users.noreply.github.com>
2022-05-17 14:31:20 +0200

    Inizio lavoro su comandi /nuova e /mostra

--- src/main/java/it/uniba/app/parser/Comando.java
+++ src/main/java/it/uniba/app/parser/Comando.java
@@ -1,8 +1,39 @@
 package it.uniba.app.parser;
 
+import it.uniba.app.Gioco;
+
 /**
- *
+ * Comando è una classe Control.
+ * Offre definizioni interne dei comandi accessibili dall'applicazione.
+ * 
  * @author Vito Verna - 746463
+ * @author Sergio Mari - 741336
  */
 public class Comando {
+    /**
+     * 
+     * Esecuzione effettiva del comando "/nuova [PAROLA]".
+     * 
+     * @param input String da utilizzare come parola segreta
+     * @param gioco La partita attuale (Gioco) che contiene la parola segreta che si vuole modficare
+     * 
+     * Sergio Mari - 741336
+     */
+    //TODO Decidere se eseguire i controlli qui o all'esterno
+    public void nuova(String input, Gioco gioco){
+        gioco.setParolaSegreta(input);
+    }
+
+    /**
+     * Esecuzione effettiva del comando "/mostra".
+     * 
+     * @param gioco La partita attuale che contiene la parola segreta che si vuole mostrare
+     * @return La parola segreta come String
+     * 
+     * Sergio Mari - 741336
+     */
+    public String mostra(Gioco gioco){
+        return gioco.getParolaSegreta();
+    }
+
 }
