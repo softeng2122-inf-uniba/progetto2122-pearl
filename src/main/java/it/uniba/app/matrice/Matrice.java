53c30bca730046f2b5744d7a0ba557ff1fa36abf
Alessandro <a.mazzotta15@studenti.uniba.it>
2022-05-18 19:16:42 +0200

    Implementati metodi stampaMatrice, setCella e resetMatrice Ãper la classe Matrice

--- src/main/java/it/uniba/app/matrice/Matrice.java
+++ src/main/java/it/uniba/app/matrice/Matrice.java
@@ -39,4 +39,49 @@ public class Matrice {
             }
         }
     }
+
+    /**
+     * Metodo che si occupa della stampa della matrice
+     * usando il toString() di Cella.
+     */
+    public void stampaMatrice() {
+        int i;
+        int j;
+        System.out.println();
+        for (i = 0; i < RIGHE; i++) {
+            for (j = 0; j < COLONNE; j++) {
+                System.out.print(mat[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * Metodo che si occupa di impostare il colore e la lettera
+     * della cella in posizione ixj.
+     *
+     * @param i - indice di riga
+     * @param j - indice della colonna
+     * @param colore - colore da impostare
+     * @param lettera - lettera da impostare
+     */
+    public void setCella(final int i, final int j, final int colore,
+            final char lettera) {
+        mat[i][j].setColore(colore);
+        mat[i][j].setLettera(lettera);
+    }
+
+    /**
+     * Metodo che si occupa di reimpostare la matrice a valori vuoti.
+     */
+    public void resetMatrice() {
+        int i;
+        int j;
+
+        for (i = 0; i < RIGHE; i++) {
+            for (j = 0; j < COLONNE; j++) {
+                setCella(i, j, 0, ' ');
+            }
+        }
+    }
 }
