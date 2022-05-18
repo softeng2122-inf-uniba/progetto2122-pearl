ee7ae684cc1e0fad0295e3b098af08aaa6c2f2e0
Verna-Vito <v.verna5@studenti.uniba.it>
2022-05-18 19:13:53 +0200

    Creati attributi e implementato costruttore per la classe Matrice

--- src/main/java/it/uniba/app/matrice/Matrice.java
+++ src/main/java/it/uniba/app/matrice/Matrice.java
@@ -1,8 +1,42 @@
 package it.uniba.app.matrice;
 
 /**
+ * Matrice è una classe <<Entity>>.
+ * Rappresenta la matrice di gioco.
  *
+ * @author Alessandro Mazzotta - 766414
  * @author Vito Verna - 746463
  */
 public class Matrice {
+
+    /**
+     * Costante di tipo int; identifica il numero di righe della matrice.
+     */
+    public static final int RIGHE = 6;
+
+    /**
+     * Costante di tipo int; identifica il numero di colonne della matrice.
+     */
+    public static final int COLONNE = 5;
+
+    /**
+     * Attributo di tipo Cella per la creazione della matrice.
+     */
+    private final Cella[][] mat;
+
+    /**
+     * La matrice viene inizializzata da un insieme di Celle vuote,
+     * con una dimensione: RIGHExCOLONNE.
+     */
+    public Matrice() {
+        mat = new Cella[RIGHE][COLONNE];
+        int i;
+        int j;
+
+        for (i = 0; i < RIGHE; i++) {
+            for (j = 0; j < COLONNE; j++) {
+                mat[i][j] = new Cella();
+            }
+        }
+    }
 }
