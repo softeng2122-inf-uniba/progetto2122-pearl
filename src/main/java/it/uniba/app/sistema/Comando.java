d18072a4552537c718bc5af723262a1aedc44c87
SSUPII <42090377+SSUPII@users.noreply.github.com>
2022-05-18 15:35:05 +0200

    Risolti problemi checkstyle per Comando

--- src/main/java/it/uniba/app/sistema/Comando.java
+++ src/main/java/it/uniba/app/sistema/Comando.java
@@ -5,24 +5,26 @@ import it.uniba.app.Gioco;
 /**
  * Comando è una classe <<Control>>.
  * Offre definizioni interne dei comandi accessibili dall'applicazione.
- * 
+ *
  * @author Vito Verna - 746463
  * @author Sergio Mari - 741336
  */
 public class Comando {
     /**
-     * 
+     *
      * Esecuzione effettiva del comando "/nuova [PAROLA]".
-     * 
+     *
      * @param input - [PAROLA] String da utilizzare come parola segreta
-     * @param gioco - La partita attuale (Gioco) che contiene la parola segreta che si vuole modificare
-     * 
-     * @return Accettabilità della parola input. 
+     * @param gioco
+     *  - La partita (Gioco) che contiene la parola segreta da modificare
+     *
+     * @return Accettabilità della parola input.
      * @see Parser#parseParola(String)
      */
-    public int nuova(String input, Gioco gioco){
+    public int nuova(final String input, final Gioco gioco) {
         int stato = new Parser().parseParola(input);
-        if(stato == Parser.IDsParole.ACCETTABILE.id){
+
+        if (stato == Parser.IDsParole.ACCETTABILE.id) {
             gioco.setParolaSegreta(input);
         }
         return stato;
@@ -30,11 +32,11 @@ public class Comando {
 
     /**
      * Esecuzione effettiva del comando "/mostra".
-     * 
-     * @param gioco - La partita attuale che contiene la parola segreta che si vuole mostrare
+     *
+     * @param gioco - La partita che contiene la parola segreta da mostrare
      * @return La parola segreta come String
      */
-    public String mostra(Gioco gioco){
+    public String mostra(final Gioco gioco) {
         return gioco.getParolaSegreta();
     }
 
