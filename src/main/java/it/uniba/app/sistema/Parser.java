bce99c6a7e005cf01fde9b40f54b7a87ee5f2488
Verna-Vito <v.verna5@studenti.uniba.it>
2022-06-01 19:54:28 +0200

    Rimozione del metodo parseTentativi dalla classe Parser

--- src/main/java/it/uniba/app/sistema/Parser.java
+++ src/main/java/it/uniba/app/sistema/Parser.java
@@ -1,16 +1,10 @@
 package it.uniba.app.sistema;
 
 import java.util.StringTokenizer;
-import it.uniba.app.matrice.Cella;
-import it.uniba.app.matrice.Matrice;
-
-import java.util.Map;
-import java.util.HashMap;
 
 import it.uniba.app.Gioco;
 import it.uniba.app.enumerativi.IDsComandi;
 import it.uniba.app.enumerativi.IDsParole;
-import it.uniba.app.enumerativi.IDsColori;
 
 /**
  * Parser e' una classe <<Control>>.
@@ -133,75 +127,5 @@ public class Parser {
         }
 
         return risultato;
-    }
-
-    /**
-     * Effettua un controllo sulla parola inserita ad ogni tentativo
-     * e assegna ad ogni carattere il rispettivo colore.
-     *
-     * @param tentativi effettuati dall'utente a ogni inserimento
-     * @param gioco usato per accedere al valore dei tentativi massimi.
-     * @param input Stringa inserita dall'utente per indovinare la parola
-     *
-     * @return numero tentativi effettuati
-     */
-
-    public Cella[] parseTentativi(final int tentativi, final Gioco gioco,
-    final String input) {
-        Map<Character, Integer> dizionario = new HashMap<>();
-        char[] charInput = new char[Matrice.COLONNE];
-        char[] charParola = new char[Matrice.COLONNE];
-        int[] arr = new int[input.length()];
-
-        if ((gioco.getTentativiMassimi()) >= tentativi) {
-            for (byte i = 0; i < input.length(); i++) {
-                charInput[i] = input.charAt(i);
-                charParola[i] = gioco.getParolaSegreta().charAt(i);
-                if (dizionario.containsKey(
-                        gioco.getParolaSegreta().charAt(i))) {
-                    dizionario.replace(
-                        gioco.getParolaSegreta().charAt(i),
-                        dizionario.get(gioco.getParolaSegreta().charAt(i)) + 1);
-                } else {
-                    dizionario.put(charParola[i], 1);
-                }
-            }
-
-            for (byte i = 0; i < input.length(); i++) {
-                if (charInput[i] == charParola[i]) {
-                    dizionario.replace(charParola[i],
-                    dizionario.get(charParola[i]) - 1);
-                   arr[i] = IDsColori.VERDE.getId();
-                }
-            }
-            for (byte i = 0; i < input.length(); i++) {
-                 if (dizionario.containsKey(charInput[i])) {
-                     if (arr[i] != IDsColori.VERDE.getId()) {
-                        if (dizionario.get(charInput[i]) != 0) {
-                            dizionario.replace(charInput[i],
-                            dizionario.get(charInput[i]) - 1);
-                            arr[i] = IDsColori.GIALLO.getId();
-                        } else {
-                            arr[i] = IDsColori.GRIGIO.getId();
-                        }
-                    }
-                } else {
-                    arr[i] = IDsColori.GRIGIO.getId();
-                }
-            }
-        }
-        Cella[] array = new Cella[Matrice.COLONNE];
-        for (int i = 0; i < input.length(); i++) {
-            array[i] = new Cella();
-            array[i].setLettera(charInput[i]);
-            if (arr[i] == IDsColori.VERDE.getId()) {
-                array[i].setColore(IDsColori.VERDE.getId());
-            } else if (arr[i] == IDsColori.GIALLO.getId()) {
-                array[i].setColore(IDsColori.GIALLO.getId());
-            } else if (arr[i] == IDsColori.GRIGIO.getId()) {
-                array[i].setColore(IDsColori.GRIGIO.getId());
-            }
-        }
-        return array;
     }
 }
