0d98863489de8850f8d2b685119241843f0fda69
Verna-Vito <v.verna5@studenti.uniba.it>
2022-06-01 18:37:08 +0200

    Modifica Javadoc per package-info

--- src/main/java/it/uniba/app/enumerativi/package-info.java
+++ src/main/java/it/uniba/app/enumerativi/package-info.java
@@ -1,4 +1,11 @@
 /**
- * Pakcage che contiene gli enumerativi dell'applicazione.
+ * Package che contiene le classi enumerative utili al programma per garantire
+ * la corretta esecuzione.
+ * Contine le classi:
+ * <ul>
+ * <li>IDsColori;</li>
+ * <li>IDsComandi;</li>
+ * <li>IDsParole.</li>
+ * </ul>
  */
 package it.uniba.app.enumerativi;
